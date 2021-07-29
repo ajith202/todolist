246fb3192098651277c47f86f84f2e861cb9a928
ajith202 <ajithantony9512@gmail.com>
2021-07-29 20:54:37 +0530

    my first commit

---
+++ todoapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import TodoListItem
+from django .http import HttpResponseRedirect
+
+
+def todoappView(request):
+    all_todo_items =TodoListItem.objects.all()
+    return render(request,'todolist.html',{'all_items':all_todo_items})
+
+def addTodoView(request):
+    x= request.POST['content']
+    new_item = TodoListItem(content= x)
+    new_item.save()
+    return HttpResponseRedirect('/todoapp')
